c0a8c051a69cadba6d55a056229a077e5cac527c
EstebanNavas <arcoesteb@live.com>
2025-01-09 20:44:31 -0500

    Inicio de proyecto

---
+++ src/main/java/com/contabook/ContabookApplication.java
@@ -0,0 +1,13 @@
+package com.contabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabookApplication.class, args);
+	}
+
+}
